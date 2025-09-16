7acc2b53c4e1bb50c983c6eabe69a9c07f0d23d4
Misael Alves <112561288+misaelalves99@users.noreply.github.com>
2025-09-16 15:35:00 -0300

    Atualizando database

--- config/database.php
+++ config/database.php
@@ -30,7 +30,7 @@ return [
             'transaction_mode' => 'DEFERRED',
         ],
 
-        'mysql' => [
+            'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -46,8 +46,9 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => base_path(env('DB_SSL_CA')),
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+                // só usa SSL se a variável existir no .env
+                PDO::MYSQL_ATTR_SSL_CA => env('DB_SSL_CA', null),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('DB_SSL_VERIFY', false),
             ]) : [],
         ],
 
